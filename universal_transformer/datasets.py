04e1dd44eb27ad9683ed1fcc3150a7a587147fa1
Dave Decker <dave.decker@gmail.com>
2021-05-02 01:55:34 -0500

    print

--- universal_transformer/datasets.py
+++ universal_transformer/datasets.py
@@ -148,7 +148,7 @@ def texts_to_tensors_lambada(texts, vocab, split=False, keep=None, debug=False):
     return TensorDataset(vec2D, masks)
 
 def get_dataset(config, tokenizer=None):
-    print('config', config)
+    print('config', config.dataset)
     key = ("dataset", config.dataset)
     if key in registry:
         cls, kwargs = registry[key]
