8cdf2d22ba30ccb1beb4a9bb7105c6564fff55af
Dave Decker <dave.decker@gmail.com>
2021-05-03 00:42:50 -0500

    print size(0)

--- universal_transformer/models.py
+++ universal_transformer/models.py
@@ -49,8 +49,8 @@ class TransformerModelBase(nn.Module):
         source_ids = source_ids.permute(1, 0, 2)
         target_ids = target_ids.permute(1, 0, 2)
 
+        print(target_ids.size(0))
         att_mask = self.transformer.generate_square_subsequent_mask(
-            print(target_ids.size(0))
             target_ids.size(0)
         )
         att_mask = att_mask.to(source_ids.device)
