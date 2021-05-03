bce99c08246f6ba57d3606c96b65fd0a5224233a
Dave Decker <dave.decker@gmail.com>
2021-05-03 12:48:22 -0500

    loss to numpy

--- train_lambada.py
+++ train_lambada.py
@@ -105,10 +105,10 @@ def run_model_on_dataset(
         ):
             #logits = np.concatenate(logits, axis=0)
             #yield logits, preds, label_ids, total_loss / batches_since_yield
-            mean_loss = total_loss / sum_of_words 
+            mean_loss = (total_loss / sum_of_words).detach().cpu().numpy().sum() 
             perplexity = np.exp(mean_loss.detach().cpu().numpy())
             accuracy = correct / total_examples
-            mean_target_loss = (total_target_loss / total_examples).detach().cpu().numpy().sum()
+            mean_target_loss = total_target_loss / total_examples
             target_perplexity = np.exp(mean_target_loss)
             yield  mean_loss, perplexity, accuracy, target_perplexity #batches_since_yield
             total_loss = 0
